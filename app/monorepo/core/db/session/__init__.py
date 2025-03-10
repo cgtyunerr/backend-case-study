45f5cbdab89052eb82a903a003dcf4c693d8d64c
cgtyunerr <110602817+cgtyunerr@users.noreply.github.com>
2025-03-11 02:48:58 +0300

    feat(app): add database logic and add database url computer (#7)

---
+++ app/monorepo/core/db/session/__init__.py
@@ -0,0 +1,7 @@
+"""Database session module."""
+
+from session import database_session_manager
+
+__all__ = [
+    "database_session_manager",
+]
---
+++ app/monorepo/core/db/session/session.py
@@ -0,0 +1,36 @@
+"""Database session module."""
+
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.settings import settings
+
+from .base_session import BaseSessionManager
+
+
+class DatabaseSessionManager(BaseSessionManager):
+    """Database session manager.
+
+    Methods:
+        get_session: Get a new db session.
+    """
+
+    @asynccontextmanager
+    async def get_session(self) -> AsyncGenerator[AsyncSession, None]:
+        """Provide database session for transaction."""
+        async with self.async_session() as session:
+            try:
+                yield session
+                await session.commit()
+            except Exception as exception:
+                await session.rollback()
+                raise exception from exception
+            finally:
+                await session.close()
+
+
+database_session_manager: DatabaseSessionManager = DatabaseSessionManager(
+    str(settings.DB.db_url)
+)
